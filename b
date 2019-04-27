de4d181cf643c3133b1c59866cf818e74615a642
Yijun Yu <y.yu@open.ac.uk>
2019-04-27 17:21:48 +0100

    update

---
+++ b
@@ -0,0 +1,3 @@
+make fast-tf
+docker images yijun/fast:tf
+docker run --rm --entrypoint sh -it yijun/fast:tf
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+if [ 0 ]; then
+	docker build -t yijun/fast:apk_builder builder
+	docker push yijun/fast:apk_builder
+fi
 make build
-#make fast
+make fast
 make upload
